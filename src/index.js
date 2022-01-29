d2177f82175066d1dafd865cba3123793e2e82ac
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-29 21:53:23 +0600

    Layout Design

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./assets/css/grid.css";
+import "./assets/css/index.css";
+import "./assets/css/theme.css";
+import Layout from "./components/layout/Layout";
+
+document.title = "Admin Dashboard";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
